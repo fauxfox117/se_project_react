7570b8b5804987f2fb5eab86ef096e673cf109ea
fauxfox117 <SABolin1@me.com>
2025-09-21 12:43:48 -0400

    open and close function working

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,11 +1,25 @@
 import "./ModalWithForm.css";
 
-function ModalWithForm({ children, buttonText, title }) {
+function ModalWithForm({
+  children,
+  buttonText,
+  title,
+  activeModal,
+  handleCloseClick,
+}) {
   return (
-    <div className="modal">
+    <div
+      className={`modal ${
+        activeModal === "add-garment" ? "modal__opened" : ""
+      }`}
+    >
       <div className="modal__content">
         <h2 className="modal__title">{title}</h2>
-        <button className="modal__close-btn" type="button">
+        <button
+          onClick={handleCloseClick}
+          className="modal__close-btn"
+          type="button"
+        >
           CLOSE
         </button>
         <form className="modal__form">
