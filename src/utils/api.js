99184e3e149f9cf07e93fa0e91ede9f42ccb5f4a
fauxfox117 <SABolin1@me.com>
2025-10-25 19:04:27 -0400

    working on delete/post function

--- src/utils/api.js
+++ src/utils/api.js
@@ -9,3 +9,18 @@ const handleServerResponse = (res) => {
 export const getItems = () => {
   return fetch(`${baseUrl}/items`, { headers }).then(handleServerResponse);
 };
+
+export const addItem = ({ name, imageUrl, weather }) => {
+  return fetch(`${baseUrl}/items`, {
+    method: "POST",
+    headers,
+    body: JSON.stringify({ name, imageUrl, weather }),
+  }).then(handleServerResponse);
+};
+
+export const removeItem = (itemId) => {
+  return fetch(`${baseUrl}/items/${itemId}`, {
+    method: "DELETE",
+    headers,
+  }).then(handleServerResponse);
+};
